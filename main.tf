28092b548c56bdc18ad88c6bf117a6a0ac4f8569
sandeep <munagalasandeepreddy@gamail.com>
2024-10-26 12:33:38 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ resource "aws_security_group" "sg" {
 
 resource "aws_instance" "instance" {
   ami = data.aws_ami.ami.id
-  instance_type = "t3-small"
+  instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
   iam_instance_profile = aws_iam_instance_profile.instance_profile.name
 
